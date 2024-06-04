3dc86586973643751e94d2a3c46c827f74ef6109
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-04 21:07:32 +0300

    betty errors removed

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary
+ * tree using post-order traversal
  * @tree:pointer ti the node of tree to transverse
  * @func: pointer fi function to cll for each node
  *
