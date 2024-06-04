b57bd20eedfe174331535fc21a80cdd359592f98
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-04 15:46:56 +0300

    removed betty errors

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,12 +11,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t* newNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
 	if (newNode == NULL)
-		return NULL;
+		return (NULL);
 	newNode->parent = parent;
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
-	return newNode;
+	return (newNode);
 }
