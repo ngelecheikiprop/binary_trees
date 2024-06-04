a3252096ee49772223d99bd6685819b5ae988c7c
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-04 18:03:00 +0300

    removed betty errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,14 @@
 #include "binary_trees.h"
 
-	/**
-	 * binary_tree_delete - deletes a binary tree
-	 * you worked so hard for
-	 *
-	 * @tree: pointer to the root node
-	 *
-	 * Return: it doesnt even care to return anything
-	 */
-	void binary_tree_delete(binary_tree_t *tree)
+/**
+ *binary_tree_delete - deletes a binary tree
+ *you worked so hard for
+ *
+ * @tree: pointer to the root node
+ *
+ * Return: it doesnt even care to return anything
+ */
+void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
