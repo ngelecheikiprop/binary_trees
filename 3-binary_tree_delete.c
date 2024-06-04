2388554a30c4145902bd202ccf78af89ded1d041
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-04 17:59:48 +0300

    number 3 done

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
-/**
- * binary_tree_delete - deletes a binary tree
- * you worked so hard for
- *
- * @tree: pointer to the root node
- *
- * Return: it doesnt even care to return anything
- */
-void binary_tree_delete(binary_tree_t *tree)
+	/**
+	 * binary_tree_delete - deletes a binary tree
+	 * you worked so hard for
+	 *
+	 * @tree: pointer to the root node
+	 *
+	 * Return: it doesnt even care to return anything
+	 */
+	void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
 	binary_tree_delete(tree->left);
-	free(tree);
 	binary_tree_delete(tree->right);
+	free(tree);
 }
