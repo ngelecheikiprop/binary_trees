ecfca259bc9f6e8d2161376047d84b23db500327
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-05 14:29:03 +0300

    number 13 upload

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+/**
+ * binary_tree_nodes - counts the nodes with atleast 1 child
+ *
+ * @tree: pointer to the root node of the tree to count
+ *
+ *Return:number of node with 1 child
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ *
+ * @tree: pointer to the node of the tree to measure size
+ *Return: return 0
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t lsize = 0;
+	size_t rsize = 0;
+
+	if (tree == NULL)
+		return (0);
+	lsize = binary_tree_size(tree->left);
+	rsize = binary_tree_size(tree->right);
+	return (lsize + rsize + 1);
+}
+/**
+ * binary_tree_leaves - counts leaves in a binary tree
+ *
+ * @tree: pointer to root node to count the number
+ * of leaves
+ *
+ * Return: number of leaves and 0 if tree is null
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t rleaves = 0;
+	size_t lleaves = 0;
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+
+	if (tree->right)
+		rleaves = binary_tree_leaves(tree->right);
+	if (tree->left)
+		lleaves = binary_tree_leaves(tree->left);
+	return (rleaves + lleaves);
+}
