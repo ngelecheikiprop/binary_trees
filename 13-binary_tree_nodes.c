97d7f1f4c54ca7e53638d18e9b3bc11db06dff39
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-05 15:24:45 +0300

    added check for if tree is null

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,6 +11,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
 
