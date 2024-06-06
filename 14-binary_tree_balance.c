5c325e04ee4c74562f5a78fd2c26d3bfd059aca4
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-06 12:55:19 +0300

    added a check for null before procedding to business

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,6 +11,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int left_height = 0;
 	int right_height = 0;
 
+	if (tree == NULL)
+		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return (left_height - right_height);
