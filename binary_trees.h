984d1390c4e8af185b05c545771f00559f954cce
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-06 12:46:06 +0300

    removed betty erros in header file

--- binary_trees.h
+++ binary_trees.h
@@ -38,7 +38,7 @@ typedef struct binary_tree_s heap_t;
  *
  * function prototypes
  *
- * */
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -57,5 +57,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
