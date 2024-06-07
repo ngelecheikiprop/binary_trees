4572ed0e90f776f890b8d1bcbf027999711ff206
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-06-07 07:33:32 +0300

    removed betty errors

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left != NULL && tree->right!= NULL)
+	if (tree->left != NULL && tree->right != NULL)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	return (0);
 }
